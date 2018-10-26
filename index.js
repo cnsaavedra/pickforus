bb17b56f043fe63be86f2fda891a7dd7dc08b2c4
Christian <Christian>
2018-10-26 14:47:08 -0400

    moving on to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:3000/chat_database';
+
+
+MongoClient.connect(url, function (err, db){
+    var messagesCollection = db.collection('messages');
+
+
+    const tech = io.of('/tech');
+    var topWords = {};
+
+
+    tech.on('connection', (socket)=>{
+        socket.on('join', (data) =>{
+            socket.join(data.room);
+            tech.in(data.room).emit('message', 'New User Connected');
+        });
+
+        socket.on('message', (data)=>{
+
+            messagesCollection.insertOne({text: data.msg}, function (err, res){
+               console.log('inserted into messagesCollection')
+            });
+            tech.in(data.room).emit('message', data.msg);
+            console.log(data.msg + " outside messageCollection");
+        });
+
+        io.on('disconnect', ()=>{
+            console.log('User Disconnected');
+            tech.emit('message', 'User Disconnected');
+        });
+
+
+    });
 
 const port = 3000;
 
@@ -24,21 +58,5 @@ app.get('/pickforusplease', (req, res)=>{
 });
 
 
-const tech = io.of('/tech');
-
-tech.on('connection', (socket)=>{
-    socket.on('join', (data) =>{
-        socket.join(data.room);
-        tech.in(data.room).emit('message', 'New User Connected');
-    });
-
-    socket.on('message', (data)=>{
-        tech.in(data.room).emit('message', data.msg);
-    });
-
-    io.on('disconnect', ()=>{
-        console.log('User Disconnected');
-        tech.emit('message', 'User Disconnected');
-    });
 
 });
