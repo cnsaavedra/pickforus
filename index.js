5eef9f45d3af9ff9a7d73c99ffbbef9cf116917e
cnsaavedra <christian-saavedra@hotmail.com>
2018-10-27 15:36:27 -0400

    manipulated data to narrow down food options

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/chat_database';
 
 
-MongoClient.connect(url, function (err, db){
+MongoClient.connect(url, function (err, db) {
     var messagesCollection = db.collection('messages');
 
 
@@ -13,24 +13,37 @@ MongoClient.connect(url, function (err, db){
     var topWords = {};
 
 
-    tech.on('connection', (socket)=>{
-        socket.on('join', (data) =>{
+    tech.on('connection', (socket) => {
+        socket.on('join', (data) => {
             socket.join(data.room);
             tech.in(data.room).emit('message', 'New User Connected');
         });
 
-        socket.on('message', (data)=>{
-
-            messagesCollection.insertOne({text: data.msg}, function (err, res){
-               console.log('inserted into messagesCollection')
-            });
-            //#TODO now that we have database, need to manipulate database
+        socket.on('message', (data) => {
+            if (data.msg.trim().indexOf(' ') == -1) {
+                messagesCollection.insertOne({text: data.msg}, function (err, res) {
+                    console.log(data.msg + ' inserted into messagesCollection')
+                });
+            }
+            else { // sentence includes more than one word
+                var words = data.msg.split(" ");
+                for (var i = 0; i < words.length; i++) {
+                    console.log(words[i] + ' are the split messages');
+                    if (words[i].length <= 3) {
+                        console.log(words[i] + ' too short to be a food name')
+                    }
+                    else if (words[i].length >= 3 && words[i] !== "want" && words[i] !== "some" && words[i] !== "have" && isNaN(words[i])) {
+                        messagesCollection.insertOne({text: words[i]}, function (err, res) {
+                        });
+                        console.log(words[i] + ' might be food so added to database')
+                    }
+                }
+            }
             tech.in(data.room).emit('message', data.msg);
-            tech.in(data.room).emit('message', data.msg + "The number of times " + db.collection())
-            console.log(data.msg + " outside messageCollection");
         });
 
-        io.on('disconnect', ()=>{
+
+        io.on('disconnect', () => {
             console.log('User Disconnected');
             tech.emit('message', 'User Disconnected');
         });
@@ -38,27 +51,26 @@ MongoClient.connect(url, function (err, db){
 
     });
 
-const port = 3000;
+    const port = 3000;
 
-server.listen(port, ()=>{
-    console.log('Server is listening on Port: ${port}');
-});
-
-
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/food', (req, res)=>{
-    res.sendFile(__dirname + '/public/food.html');
-});
-app.get('/destination', (req, res)=>{
-    res.sendFile(__dirname + '/public/destination.html');
-});
-app.get('/pickforusplease', (req, res)=>{
-    res.sendFile(__dirname + '/public/pickforusplease.html');
-});
+    server.listen(port, () => {
+        console.log('Server is listening on Port: ${port}');
+    });
 
 
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/public/index.html');
+    });
+
+    app.get('/food', (req, res) => {
+        res.sendFile(__dirname + '/public/food.html');
+    });
+    app.get('/destination', (req, res) => {
+        res.sendFile(__dirname + '/public/destination.html');
+    });
+    app.get('/pickforusplease', (req, res) => {
+        res.sendFile(__dirname + '/public/pickforusplease.html');
+    });
 
 });
+
