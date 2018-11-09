e1654127b125144c1d1f14aae234a8579709ae61
cnsaavedra <christian-saavedra@hotmail.com>
2018-11-09 14:34:07 -0500

    top3 difficulties, messy clean up cold (need _id of MyResult)

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ MongoClient.connect(url, function (err, db) {
 
     const tech = io.of('/tech');
     var connections = [];
-    var timer = 5;
-    var topWords = [];
-
+    var timer = 2;
 
 
     tech.on('connection', (socket) => {
@@ -27,10 +25,8 @@ MongoClient.connect(url, function (err, db) {
         //FOR TIMER
         var ChatCountdown = setInterval(function(){
             socket.emit('time_server', timer);
-            if(timer !== 0){
-                timer--;
-            }
-            else if(timer === 0 || timer < 0){
+            timer--; // had to remove the if (timer > 0) because it creates two multiples of else if(timer === 0)(figure out why later)
+            if(timer === 0 || timer < 0){
                 socket.emit('time_server', timer);
                 clearInterval(ChatCountdown);
             }
@@ -84,7 +80,14 @@ MongoClient.connect(url, function (err, db) {
 
     const port = 3000;
     var topFood = 0;
+    var topFood2 = 0;
+    var topFood3 = 0;
     var topFoodArray = 0;
+    var top2FoodArray = 0;
+    var top3FoodArray = 0;
+    var top3 = [];
+    var top3Arrays = [];
+
 
     server.listen(port, () => {
         console.log('Server is listening on Port: ${port}');
@@ -93,14 +96,36 @@ MongoClient.connect(url, function (err, db) {
     app.get('/get-data', function(req, res, next) {
         db.collection("messages", function (err, collection) {
             collection.aggregate([{$group: {_id: "$text", MyResults: {$sum: 1}}}]).toArray(function (err, data) {
+
+                //puts all the count of each food in an array through numbers
                 for(var i =0; i < data.length; i++){
                     if(data[i].MyResults > topFood){
                         topFoodArray = i;
                     }
+                    top3Arrays.push(data[i].MyResults)
                 }
+                //#TODO SHIT IT MESSY HERE TRY TO GET TOP 3
+                console.log("All Counts: " + top3Arrays);
+                // this gets the MyResult top 3 (not the id)
+                var topValues = top3Arrays.sort((a,b) => a<b).slice(0,3);
+                //now that got top 3 MyResult, get its respective id
+                console.log("Top 3 Counts: " + topValues);
+                console.log("Top 1 Count respective MyResult: " + topValues[0]);
+
+                for(var y=0; y<topValues.length; y++){
+                    top3[y] = data[y];
+                    console.log("HERE "+data[topValues[y]]);
+                }
+
+
                 topFood = data[topFoodArray]._id;
-                console.log("Top Food: " + topFood);
-                res.json(topFood);
+                topFood2 = data[top2FoodArray]._id;
+                topFood3 = data[top3FoodArray]._id;
+                top3[0] = topFood;
+                top3[1] = topFood2;
+                top3[2] = topFood3;
+                console.log("Top Food: " + top3[0]);
+                res.json(top3);
             })
         });
     });
